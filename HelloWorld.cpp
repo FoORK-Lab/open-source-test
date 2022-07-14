d671f8c142a52017f58338bcb866cba03f2bc819
KARTHIK LAL <karthik.lal558@gmail.com>
2022-07-14 11:50:18 +0530

    OPEN-SOURCE-TEST: create Earn_Pair_Extraordinare file.cpp
    > Earn Pair_Extraordinare badge using this trick [2]
    
    Co-authored-by: KARTHIK LAL <karthik.lal558@gmail.com>
    Co-authored-by: Bhavya Krishna <82134893+bhavyakrishna001@users.noreply.github.com>

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+    int main()
+    {
+        cout << "Hello World!" << endl;
+    }
+    clrscr ();
+    getch();
+    // End of file.
+    // Compile: g++ HelloWorld.cpp -o HelloWorld.exe
+    return 0;
+    }
